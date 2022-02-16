46ec4985934206e95564fadeeab2243f7963cfc0
Chris Cassano <chris@litprotocol.com>
2022-02-16 00:12:02 -0700

    upgrade sdks, and add a commented out test of evmContractConditions

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ declare global {
   }
 }
 
-let litCeramicIntegration = new Integration()
+let litCeramicIntegration = new Integration('https://ceramic-clay.3boxlabs.com', 'polygon')
 
 let streamID = 'kjzl6cwe1jw1479rnblkk5u43ivxkuo29i4efdx1e7hk94qrhjl0d4u0dyys1au' // test data
 
@@ -53,6 +53,7 @@ document.getElementById('readCeramic')?.addEventListener('click', () => {
 // encrypt with Lit and write to ceramic
 
 document.getElementById('encryptLit')?.addEventListener('click', function () {
+  console.log('chain in litCeramicIntegration: ', litCeramicIntegration.chain)
   // @ts-ignore
   const stringToEncrypt = document.getElementById('secret').value
   // User must posess at least 0.000001 ETH on eth
@@ -73,4 +74,62 @@ document.getElementById('encryptLit')?.addEventListener('click', function () {
     .encryptAndWrite(stringToEncrypt, accessControlConditions)
     .then((value) => updateStreamID(value))
   console.log(response)
+
+  // const evmContractConditions = [
+  //   {
+  //     contractAddress: '0xb71a679cfff330591d556c4b9f21c7739ca9590c',
+  //     functionName: 'members',
+  //     functionParams: [':userAddress'],
+  //     functionAbi: {
+  //       constant: true,
+  //       inputs: [
+  //         {
+  //           name: '',
+  //           type: 'address',
+  //         },
+  //       ],
+  //       name: 'members',
+  //       outputs: [
+  //         {
+  //           name: 'delegateKey',
+  //           type: 'address',
+  //         },
+  //         {
+  //           name: 'shares',
+  //           type: 'uint256',
+  //         },
+  //         {
+  //           name: 'loot',
+  //           type: 'uint256',
+  //         },
+  //         {
+  //           name: 'exists',
+  //           type: 'bool',
+  //         },
+  //         {
+  //           name: 'highestIndexYesVote',
+  //           type: 'uint256',
+  //         },
+  //         {
+  //           name: 'jailed',
+  //           type: 'uint256',
+  //         },
+  //       ],
+  //       payable: false,
+  //       stateMutability: 'view',
+  //       type: 'function',
+  //     },
+  //     chain: 'xdai',
+  //     returnValueTest: {
+  //       key: 'shares',
+  //       comparator: '>=',
+  //       value: '1',
+  //     },
+  //   },
+  // ]
+
+  // const response = litCeramicIntegration
+  //   .encryptAndWrite(stringToEncrypt, evmContractConditions, 'evmContractConditions')
+  //   .then((value) => updateStreamID(value))
+  // console.log(response)
 })
